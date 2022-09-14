31506f671a867caf9582a96c6a60964ece8feedd
Bohdan Karavan <bohdan_karavan@epam.com>
2022-09-14 09:50:22 +0300

    Initial commit

---
+++ src/main/java/com/pikaso/crypto/Controller.java
@@ -0,0 +1,17 @@
+package com.pikaso.crypto;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    Service service;
+
+    @GetMapping(path = "get")
+    void callService() {
+        service.callService();
+    }
+}
---
+++ src/main/java/com/pikaso/crypto/Model.java
@@ -0,0 +1,24 @@
+package com.pikaso.crypto;
+
+public class Model {
+
+    private int mins;
+
+    private double price;
+
+    public int getMins() {
+        return mins;
+    }
+
+    public void setMins(int mins) {
+        this.mins = mins;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
